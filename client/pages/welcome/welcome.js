85d5bf26c52264f92ffbef8e40283b20d163c56b
70c_100002792333_SachieW <70c_100002792333_SachieW@git.cloud.tencent.com>
2018-11-19 16:56:25 +0800

    更新欢迎页

--- client/pages/welcome/welcome.js
+++ client/pages/welcome/welcome.js
@@ -1,66 +1,93 @@
 // pages/welcome/welcome.js
 Page({
-
   /**
    * 页面的初始数据
    */
   data: {
-  
+    disable:true,
+    confirmed:false
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
-  
+  onLoad: function(options) {
+
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
-  
+  onReady: function() {
+
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
-  
+  onShow: function() {
+
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
-  
+  onHide: function() {
+
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
-  
+  onUnload: function() {
+
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
-  
+  onPullDownRefresh: function() {
+
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
-  
+  onReachBottom: function() {
+
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
-  
+  onShareAppMessage: function() {
+
+  },
+  signin: function() {
+    wx.switchTab({
+      url: '../index/index',
+      success: function(res) {
+        console.log(res)
+      },
+      fail: function(res) {
+        console.log(res)
+      },
+      complete: function(res) {
+        console.log(res)
+      },
+    })
+  },
+  confirm:function(){
+    if (this.data.confirmed==false){
+      this.setData({
+        confirmed:true,
+        disable: false
+      })
+    }else{
+      this.setData({
+        confirmed:false,
+        disable: true
+      })
+    }
   }
 })
